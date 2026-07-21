01b0f7cf63c462a4dd43679ece4300609f0ba546
Joaquim <joaquimlima0612@gmail.com>
2026-07-20 22:41:21 -0300

    Término do exercício envolvendo a biblioteca random

--- Exercícios/ex013.py
+++ Exercícios/ex013.py
@@ -1,6 +1,6 @@
 # O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. Faça um programa que leia os nomes dos quatro e mostre a ordem sorteada
 
-import random
+from random import shuffle as s # apelidando shuffer de "s"
 
 print('============== PROGRAMA APAGAR QUADRO ==============')
 nome1 = input('Digite o nome do primeiro aluno: ')
@@ -9,11 +9,11 @@ nome3 = input('Digite o nome do terceiro aluno: ')
 nome4 = input('Digite o nome do quarto aluno: ')
 
 todos_alunos = [nome1, nome2, nome3, nome4]
-random.shuffle(todos_alunos) # embaralhando os alunos
+s(todos_alunos)
 
 print(f'''
-    O primeiro aluno é {todos_alunos[0]}
-    O segundo aluno é {todos_alunos[1]}
-    O terceiro aluno é {todos_alunos[2]}
-    O quarto aluno é {todos_alunos[3]}
+    O primeiro aluno a apresentar é {todos_alunos[0]}
+    O segundo aluno a apresentar é {todos_alunos[1]}
+    O terceiro aluno a apresentar é {todos_alunos[2]}
+    O quarto aluno a apresentar é {todos_alunos[3]}
 ''')
