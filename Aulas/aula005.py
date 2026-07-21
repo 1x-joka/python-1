d299bf2198dc33d7bdbdbfb6aae220820a3245ee
Joaquim <joaquimlima0612@gmail.com>
2026-07-21 12:41:10 -0300

    Aula sobre if e else (CONDIÇÕES)

--- Aulas/aula005.py
+++ Aulas/aula005.py
@@ -1,2 +1,25 @@
 # ============= CONDIÇÕES pt.1 =============
-# -> 
+# -> if e else
+
+tempo = int(input('Quantos anos tem seu carro? '))
+
+if (tempo <= 3):
+    print('carro novo')
+else:
+    print('carro velho')
+print('--FIM--')
+
+# maneira simplificada
+
+print('carro novo' if tempo <=3 else 'carro velho')
+print('--FIM--')
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1 + n2) / 2
+print(f'A sua média foi {media:.1f}') # :.1f = 1 casa decimal
+
+if (media >= 6):
+    print('Boa média! Parabéns')
+else:
+    print('Média ruim! Estude mais')
